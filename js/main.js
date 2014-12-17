265c5fdf32e096d8c0ff0821420b897a889cf467
Yiran Mao <yiranmao@gmail.com>
2014-12-17 00:52:16 -0500

    **UNSTABLE** Commit 12/16/2014 - Implemented infinite scroll(auto loading) for retrieving older questions

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
+var MORE_FLAG = true;
+var INFINITE_SCROLL_PX_OFFSET = 150;
+var LOADING_LOCK = false;
+var cursor = "";
+
 $(document).ready(function(){
+	fetchNextPage();
+
+	$(document).scroll(function(){
+		var windowTop = $(this).scrollTop();
+		var windowBottom = windowTop + $(window).height();
+		//Infinite scroll
+		if(!LOADING_LOCK & windowBottom + INFINITE_SCROLL_PX_OFFSET > $('body').height() && MORE_FLAG){
+			console.log("trigger");
+			LOADING_LOCK = true;  //prevent the inf scroll from triggering multiple times
+			fetchNextPage();
+		}
+	});
+
 	$("#create-new-question-modal").find("button").click(function(){
 		var modal = $("#create-new-question-modal");
 		var title = modal.find("input").val();
